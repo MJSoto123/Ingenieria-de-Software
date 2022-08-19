a316275c925a7a444818bd375b315bc41b93faa3
MJSoto123 <68660162+MJSoto123@users.noreply.github.com>
2022-08-19 14:39:41 -0500

    course

---
+++ WEB/src/Domain/repository/course.repository.js
@@ -0,0 +1,12 @@
+const BaseRepository = require("./base.repository");
+
+class CourseRepository extends BaseRepository {
+  constructor(CourseDb) {
+    super(CourseDb);
+  }
+  async getAllWithoutPagination() {
+    return await this.model.find();
+  }
+}
+
+module.exports = CourseRepository
